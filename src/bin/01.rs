57ef4a3628fb46ad25f17eb5fb7d8a9a01cdd1d6
Thomas Ung <tomkimsour@hotmail.fr>
2025-12-02 19:13:38 +0100

    tmp d1 p2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,15 +2,13 @@ advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<i64> {
     let mut res = 0;
-    let acc_sum = input.lines().into_iter().fold(50, |acc, line| {
+    let _ = input.lines().into_iter().fold(50, |acc, line| {
         let (direction, number_str) = line.split_at(1);
         let parsed_number: i64 = number_str.to_string().parse::<i64>().unwrap() % 100;
-        let mut current_sum: i64 = 0;
-        if direction == "L" {
-            current_sum = acc - parsed_number;
-        } else {
-            current_sum = acc + parsed_number
-        }
+        let mut current_sum = match direction {
+            "L" => acc - parsed_number,
+            _ => acc + parsed_number,
+        };
         if current_sum > 99 {
             current_sum -= 100;
         } else if current_sum < 0 {
@@ -23,12 +21,40 @@ pub fn part_one(input: &str) -> Option<i64> {
         println!("{:?}", current_sum);
         current_sum
     });
-
     Some(res)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut res = 0;
+    let _ = input.lines().into_iter().fold(50, |acc, line| {
+        let (direction, number_str) = line.split_at(1);
+        let parsed_number: i64 = number_str.to_string().parse::<i64>().unwrap();
+        res += parsed_number / 100;
+        println!(
+            "{} / 100 = {} - > res : {} ",
+            parsed_number,
+            parsed_number / 100,
+            res
+        );
+        let parsed_number = parsed_number % 100;
+        let mut current_sum = match direction {
+            "L" => acc - parsed_number,
+            _ => acc + parsed_number,
+        };
+        if current_sum > 99 {
+            current_sum -= 100;
+            res += 1;
+            println!("Res : {:}", res);
+        } else if current_sum < 0 {
+            current_sum += 100;
+            res += 1;
+            println!("Res : {:}", res);
+        }
+
+        println!("{:?}", current_sum);
+        current_sum
+    });
+    Some(res)
 }
 
 #[cfg(test)]
@@ -44,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
