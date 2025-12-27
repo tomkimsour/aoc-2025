956ca1b6c1299e69d53f400bbe4f33cc98fe52e6
Thomas Ung <tomkimsour@gmail.com>
2025-12-27 14:46:06 +0100

    solve d2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -13,24 +13,56 @@ pub fn part_one(input: &str) -> Option<u64> {
     let res: u64 = parse(input.trim())
         .into_iter()
         .map(|(l, r)| {
+            let mut local_res = 0;
             let min: u64 = l.parse().unwrap();
             let max: u64 = r.parse().unwrap();
-            let min_len = 1 + min.checked_ilog10().unwrap() as usize;
-            if (min_len / 2)
-            for i in min..max {
+            for i in min..max + 1 {
                 let i_len = 1 + i.checked_ilog10().unwrap() as usize;
                 if !i_len.is_multiple_of(2) {
                     continue;
                 }
+                let i_str = i.to_string();
+                let (l_nb, r_nb) = i_str.split_at(i_len / 2);
+                if l_nb == r_nb {
+                    println!("{}", i);
+                    local_res += i;
+                }
             }
-            0
+            local_res
         })
         .sum();
-    Some(0)
+    Some(res)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let res: u64 = parse(input.trim())
+        .into_iter()
+        .map(|(l, r)| {
+            let mut local_res = 0;
+            let min: u64 = l.parse().unwrap();
+            let max: u64 = r.parse().unwrap();
+            for i in min..max + 1 {
+                let i_len = 1 + i.checked_ilog10().unwrap() as usize;
+                let i_str = i.to_string();
+                let mut amount_of_split = 2;
+                while amount_of_split <= i_len {
+                    let (l_nb, r_nb) = i_str.split_at(i_len / amount_of_split);
+                    let mut l_concat: String = "".to_string();
+                    for _ in 0..amount_of_split - 1 {
+                        l_concat += l_nb;
+                    }
+                    if l_concat == r_nb {
+                        println!("{}", i);
+                        local_res += i;
+                        break;
+                    }
+                    amount_of_split += 1;
+                }
+            }
+            local_res
+        })
+        .sum();
+    Some(res)
 }
 
 #[cfg(test)]
@@ -46,6 +78,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
